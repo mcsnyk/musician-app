93d186cbd853259a9ccecd08405f8b805c8ac522
Miklos Csecsi <105910607+mcsnyk@users.noreply.github.com>
2023-10-18 20:44:38 +0200

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,13 @@
 # Navigate to the application directory
 cd /home/ec2-user/musicianapp
 
+# Install pm2 globally if it's not already installed
+if ! command -v pm2 &> /dev/null; then
+  npm install pm2 -g
+fi
+
+# Install application dependencies using npm
+npm install
+
 # Start the Node.js application using pm2
 pm2 start app.js
