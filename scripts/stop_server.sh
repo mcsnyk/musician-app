7a739d3e6b9f61cf0d2cfb28fd8e5bf5692b5505
Miklos Csecsi <105910607+mcsnyk@users.noreply.github.com>
2023-10-18 21:18:34 +0200

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# Stop the Node.js application (if it's running)
-pm2 stop app.js
+# Get the process ID (PID) of the Node.js application
+PID=$(ps aux | grep "node app.js" | awk '{print $2}')
+
+# Check if the PID exists
+if [ -n "$PID" ]; then
+  echo "Stopping Node.js application..."
+  # Send a SIGINT signal to gracefully stop the application
+  kill -SIGINT $PID
+  echo "Node.js application stopped."
+else
+  echo "Node.js application is not running."
+fi
