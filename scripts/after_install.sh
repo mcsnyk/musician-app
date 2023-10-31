330ae97c854ee88ef4280a607918f951a9ba3394
Miklos Csecsi <105910607+mcsnyk@users.noreply.github.com>
2023-10-31 16:21:36 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/musician-app/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/musician-app/deploy.log
+echo 'cd /home/ec2-user/musician-app' >> /home/ec2-user/musician-app/deploy.log
 cd /home/ec2-user/musician-app >> /home/ec2-user/musician-app/deploy.log
 
 echo 'npm install' >> /home/ec2-user/musician-app/deploy.log 
