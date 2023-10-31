00c147cc0832146aeae414fb7385848eb3cd592b
Miklos Csecsi <105910607+mcsnyk@users.noreply.github.com>
2023-10-31 17:43:58 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,5 +8,7 @@ echo 'npm install' >> /home/ec2-user/musician-app/deploy.log
 npm install >> /home/ec2-user/musician-app/deploy.log
 
 echo 'pm2 install' >> /home/ec2-user/musician-app/deploy.log 
-sudo npm install pm2 -g >> /home/ec2-user/musician-app/deploy.log
-
+sudo npm install pm2 -g
+sudo ln -s "$(which node)" /sbin/node
+sudo ln -s "$(which npm)" /sbin/npm
+sudo ln -s "$(which pm2)" /sbin/pm2
